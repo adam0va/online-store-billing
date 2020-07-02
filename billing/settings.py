1eb78a575a4f7ebe3839be444ef44336d9bb4e71
Irina <irinaadamova555@gmail.com>
2020-07-03 02:44:48 +0300

    allow cors

--- billing/settings.py
+++ billing/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'billing_app',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
